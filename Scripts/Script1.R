330dbb018854e87c27966562094033b3d65ae0f1
Starsk1 <estarczewski1@sheffield.ac.uk>
2023-04-04 13:06:00 +0100

    global wrangling

--- Scripts/Script1.R
+++ Scripts/Script1.R
@@ -17,3 +17,14 @@ head(raw_data)
 ##-------------------------------------------------------------DATA WRANGLING---------------------------------------------------------------
 
 #I only want to analyse global birth rate so my next steps are to filter out unnecessary columns and locations that are not global 
+
+#first step, have only rows with global birth rate 
+
+global_data <- raw_data %>% 
+  filter(location_name=="Global")+
+  select(location_name, year_id,)
+
+global_data %>% 
+  select(location_name, year_id, val)
+
+#next step remove irrelevent columns, keeping location_name, year_id, and val
